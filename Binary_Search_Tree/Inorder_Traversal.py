a2107286165065ea5dd621c63f4d95e1e5e29223
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-02-20 21:54:17 -0500

    94, Inorder Traversal

---
+++ Binary_Search_Tree/Inorder_Traversal.py
@@ -0,0 +1,15 @@
+def inorderTraversal(self, root):
+        """
+        https://leetcode.com/problems/binary-tree-inorder-traversal/solution/
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root == None:
+            return []
+        inorder = []
+        if root.left != None:
+            inorder += self.inorderTraversal(root.left)
+        inorder.append(root.val)
+        if root.right != None:
+            inorder += self.inorderTraversal(root.right)
+        return inorder
