7b368b9af38c7b5f3b9e4b37c373bbb76ff2d942
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-03-03 22:38:48 -0500

    Completed 347 Top K Frequent Elements, Medium

--- Heap/Top_K_frequent.py
+++ Heap/Top_K_frequent.py
@@ -1,6 +1,6 @@
 def topKFrequent(self, nums, k):
         """
-        https://leetcode.com/problems/top-k-frequent-elements/
+        https://leetcode.com/problems/top-k-frequent-elements/submissions/
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
@@ -12,6 +12,10 @@ def topKFrequent(self, nums, k):
                 nums_dict[i] = -1
             else:
                 nums_dict[i] -= 1
-            print nums_dict
-        for number, count in ht.items():
-            heapq.heappush(nums_heap, (-1*count, number))
+        for number, count in nums_dict.items():
+            heapq.heappush(nums_heap, (count, number))
+        output_lst = []
+        while k > 0:
+            output_lst.append(heapq.heappop(nums_heap)[1])
+            k -= 1
+        return output_lst
