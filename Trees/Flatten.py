07b9c9dc05b2a922913019927b88af4572980e39
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-04-15 21:35:31 -0400

    114 Brute Force algorithm

---
+++ Trees/Flatten.py
@@ -0,0 +1,9 @@
+ def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        
+        '''
+            Brute Force:
+                Traverse all the elements and ap
