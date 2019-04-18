ccd51cf4ef320fa5ff69970bee2a4d3c07ffb284
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-04-18 18:41:45 -0400

    917 Reverse Only Letters, Easy

---
+++ String/reverse_letters.py
@@ -0,0 +1,15 @@
+ def reverseOnlyLetters(self, S):
+        """
+        https://leetcode.com/problems/reverse-only-letters/submissions/
+        :type S: str
+        :rtype: str
+        """
+        str_lst = [i for i in S if i.isalpha()]
+        output_str = [] 
+        print str_lst
+        for letter in S:
+            if letter.isalpha():
+                output_str.append(str_lst.pop())
+            else:
+                output_str.append(letter)
+        return "".join(output_str)
