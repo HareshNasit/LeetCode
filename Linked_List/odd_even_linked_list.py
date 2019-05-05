7a1a3acb9c320701cb24e91ff4bac4332aba993e
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-05-05 14:54:37 +0300

    328 Odd Even Linked List, Medium Brute Force

---
+++ Linked_List/odd_even_linked_list.py
@@ -0,0 +1,27 @@
+def oddEvenList(self, head):
+        """
+        https://leetcode.com/problems/odd-even-linked-list/
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #Brute Force
+        count = 1
+        curr = head
+        odd_lst = ListNode(0)
+        odd_curr = odd_lst
+        even_lst = ListNode(0)
+        even_curr = even_lst
+        last_node = None
+        while curr != None:
+            if count % 2 == 0:
+                even_curr.next = curr
+                even_curr = even_curr.next
+            else:
+                odd_curr.next = curr
+                odd_curr = odd_curr.next
+                last_node = curr
+            curr = curr.next
+            count += 1
+        odd_lst = odd_lst.next
+        last_node.next = even_lst.next
+        return odd_lst
