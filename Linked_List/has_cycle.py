0f4872f86f1f6364ce12ed2eaec863dab23b2b57
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-05-10 17:11:07 +0300

    Updated 141 Has Cycle with a faster algorithm

--- Linked_List/has_cycle.py
+++ Linked_List/has_cycle.py
@@ -1,16 +1,27 @@
- def hasCycle(self, head):
+def hasCycle(self, head):
         """
-        https://leetcode.com/problems/linked-list-cycle/submissions/
+        https://leetcode.com/problems/linked-list-cycle/
         :type head: ListNode
         :rtype: bool
         """
-        nodes = []
+        nodes = {}
         curr = head
         while curr:
             if curr not in nodes:
+                nodes[curr] = 1
                 print curr.val
-                nodes.append(curr)
             else:
+                print "HELLOOO"
                 return True
             curr = curr.next
         return False
+        # nodes = []
+        # curr = head
+        # while curr:
+        #     if curr not in nodes:
+        #         print curr.val
+        #         nodes.append(curr)
+        #     else:
+        #         return True
+        #     curr = curr.next
+        # return False
