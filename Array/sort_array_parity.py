6698f77b59bf01c64f640c1f6a29d0f75b8535c8
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-05-15 21:47:58 +0300

    905, Sort Array by Parity, Easy Fastest

--- Array/sort_array_parity.py
+++ Array/sort_array_parity.py
@@ -1,15 +1,25 @@
 def sortArrayByParity(self, A):
         """
-        https://leetcode.com/problems/sort-array-by-parity/submissions/
         :type A: List[int]
         :rtype: List[int]
         """
-        odd = []
-        even = []
+        even_index = 0
         for i in range(len(A)):
-            h = A.pop()
-            if h % 2 == 0:
-                even.append(h)
-            else:
-                odd.append(h)
-        return even + odd
+            if A[i] % 2 == 0:
+                temp = A[even_index]
+                A[even_index] = A[i]
+                A[i] = temp
+                even_index += 1
+        print A
+        return A
+
+        #Brute Force
+        # odd = []
+        # even = []
+        # for i in range(len(A)):
+        #     h = A.pop()
+        #     if h % 2 == 0:
+        #         even.append(h)
+        #     else:
+        #         odd.append(h)
+        # return even + odd
