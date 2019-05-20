ab04d40cf20808803d09cf643b0fdd34c3899e06
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-05-20 22:55:03 +0300

    985 Sum of Even Numbers afer Queries

---
+++ Array/sum_of_even_numbers_queries.py
@@ -0,0 +1,7 @@
+def sumEvenAfterQueries(self, A, queries):
+        """
+        https://leetcode.com/problems/sum-of-even-numbers-after-queries/
+        :type A: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
