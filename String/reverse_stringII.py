2e490278930d51ee1b7e12b305acdf7207a7463d
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-05-29 21:56:48 +0300

    Updated

--- String/reverse_stringII.py
+++ String/reverse_stringII.py
@@ -1,20 +1,14 @@
 def reverseStr(self, s, k):
-        """
+    """
         :type s: str
         :type k: int
         :rtype: str
         """
-        if len(s) <= k:
-            reverse = ""
+            if len(s) <= k:
+                reverse = ""
             for i in range(len(s) - 1, -1, -1):
                 reverse += s[i]
-            return reverse
+        return reverse
         else:
-            count = k
+            substring = s[:k]
             reverse = ""
-            while count != 0:
-                reverse += s[count - 1]
-                count -= 1
-            print reverse
-            reverse += s[k:]
-            return reverse
