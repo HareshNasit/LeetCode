725985c55c2ba7e4e7b74785db5008fb9789468a
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-06-23 22:22:43 +0300

    1002 Common characters

---
+++ Hash Table/common_characters.py
@@ -0,0 +1,8 @@
+def commonChars(self, A):
+        """
+        :type A: List[str]
+        :rtype: List[str]
+        """
+        #Brute Force
+        #Create a dictionary for all all the strings to track the count of letters.
+        #Compare and keep the count of each letter.
