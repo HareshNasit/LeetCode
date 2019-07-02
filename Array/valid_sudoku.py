d1644efe35be85c22fe370083a8279d8e78dba84
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-07-02 21:14:56 +0300

    Helper function for Valid Sudoku

--- Array/valid_sudoku.py
+++ Array/valid_sudoku.py
@@ -1,4 +1,4 @@
-def isValidSudoku(self, board):
+ def isValidSudoku(self, board):
         """
         https://leetcode.com/problems/valid-sudoku/
         :type board: List[List[str]]
@@ -6,3 +6,13 @@ def isValidSudoku(self, board):
         """
         #Idea: Create a method to check if an array of 9 numbers are distinct.
         # Then call the method on all the possible combinations.
+def isUnique(self,nums):
+    nums_set = set()
+    for i in nums:
+        if i not in nums_set and i != ".":
+            nums_set.add(i)
+        elif i == ".":
+            pass
+        else:
+            return False
+    return True
