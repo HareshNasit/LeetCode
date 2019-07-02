d134c59009a084ecef6de3899fe69422a70a4664
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-07-02 22:44:27 +0300

    36, Valid Sudoku, Medium

--- Array/valid_sudoku.py
+++ Array/valid_sudoku.py
@@ -1,18 +1,46 @@
- def isValidSudoku(self, board):
+def isValidSudoku(self, board):
         """
-        https://leetcode.com/problems/valid-sudoku/
+        https://leetcode.com/problems/valid-sudoku/submissions/
         :type board: List[List[str]]
         :rtype: bool
         """
         #Idea: Create a method to check if an array of 9 numbers are distinct.
         # Then call the method on all the possible combinations.
-def isUnique(self,nums):
-    nums_set = set()
-    for i in nums:
-        if i not in nums_set and i != ".":
-            nums_set.add(i)
-        elif i == ".":
-            pass
-        else:
-            return False
-    return True
+        #Rows
+        for row in board:
+            if self.isUnique(row):
+                pass
+            else:
+                return False
+            
+        #Columns
+        for index in range(9):
+            column = []
+            for row in board:
+                column.append(row[index])
+            if self.isUnique(column):
+                pass
+            else:
+                return False
+            
+        #Squares
+        for b in range(3):
+            for a in range(3):
+                square = []
+                for i in range(3): 
+                    for j in range(3):
+                        square.append(board[3*b + i][3*a + j])
+                if not self.isUnique(square):
+                    return False
+        return True
+    
+    def isUnique(self,nums):
+        nums_set = set()
+        for i in nums:
+            if i not in nums_set and i != ".":
+                nums_set.add(i)
+            elif i == ".":
+                pass
+            else:
+                return False
+        return True
