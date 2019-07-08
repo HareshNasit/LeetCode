04f59a393c8d2873c4a93f670d306cc77a7fb864
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-07-08 22:38:45 +0300

    766 Toeplitz Matrix, Easy

--- Array/toeplitz_matrix.py
+++ Array/toeplitz_matrix.py
@@ -1,7 +1,15 @@
 def isToeplitzMatrix(self, matrix):
         """
-        766
+        https://leetcode.com/problems/toeplitz-matrix/solution/
         :type matrix: List[List[int]]
         :rtype: bool
         """
-        
+        #Enumerate is used to track a counter as well as the value in the array.
+        diagnols = {}
+        for r, row in enumerate(matrix,1):
+            for c, value in enumerate(row,1):
+                if r - c not in diagnols:
+                    diagnols[r-c] = value
+                elif diagnols[r - c] != value:
+                    return False
+        return True
