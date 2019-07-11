1523ee2a8352e98702b625b56487cf9bd6adcb15
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-07-11 22:30:08 +0300

    167

---
+++ Binary_Search/Two_sumII_sorted_array.py
@@ -0,0 +1,10 @@
+def twoSum(self, numbers, target):
+        """
+        https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+        167
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #Use Binary Search to add sum and by comparing the sum 
+        # with the target, traverse the list.
