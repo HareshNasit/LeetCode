75f0d5282fc42f4de94ab69c8f188e305a0d7d2f
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-08-20 21:45:06 +0300

    374, Guess Number Higher or Lower, Easy

--- Binary_Search/guess_number.py
+++ Binary_Search/guess_number.py
@@ -1,12 +1,26 @@
 def guessNumber(self, n):
         """
+        https://leetcode.com/problems/guess-number-higher-or-lower/submissions/
         :type n: int
         :rtype: int
         """
         #Brute Force
-        for i in range(n):
-            if guess(i) == 0:
-                return i
-        return n
+        # for i in range(n):
+        #     if guess(i) == 0:
+        #         return i
+        # return n
         
         #Faster algorithm, using binary search
+        
+        left,right = 0,n
+        while left <=right:
+            mid = (left+right)//2
+            if guess(mid) == 0:
+                return mid
+            else:
+                if guess(mid) == 1:
+                    left = mid+1
+                else:
+                    right = mid -1
+        return -1
+    
