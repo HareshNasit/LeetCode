bdf380246be6290c2fba3426f2e3df26547250f3
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-08-25 21:38:17 +0300

    451, Sort Characters by Frequency, Medium revised

--- String/sort_chars_freq.py
+++ String/sort_chars_freq.py
@@ -1,18 +1,18 @@
 def frequencySort(self, s):
-    """
-    :type s: str
-    :rtype: str
-    """
-    unique = []
-    freq = {}
-    for i in s:
-        if i not in freq:
-            freq[i] = 1
-        else:
-            freq[i] += 1
-    print(sorted(freq))
-    sort=sorted(freq.iteritems(), key = lambda x : x[1], reverse= True)
-    output = ""
-    for i in sort:
-        output += i[0] * i[1]
-    return output
+        """
+        https://leetcode.com/problems/sort-characters-by-frequency/submissions/
+        :type s: str
+        :rtype: str
+        """
+        freq = {}
+        for i in s:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] += 1
+        print(sorted(freq))
+        sort=sorted(freq.iteritems(), key = lambda x : x[1], reverse= True)
+        output = ""
+        for i in sort:
+            output += i[0] * i[1]
+        return output
