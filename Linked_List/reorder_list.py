9dd2061da63c2760910c741138d943b862250ff5
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-09-16 21:08:17 -0400

    143 Reorder List, Medium

--- Linked_List/reorder_list.py
+++ Linked_List/reorder_list.py
@@ -1,9 +1,13 @@
-def reorderList(self, head):
+ def reorderList(self, head):
         """
         https://leetcode.com/problems/reorder-list/
         :type head: ListNode
         :rtype: None Do not return anything, modify head in-place instead.
         """
+        """ Runtime O(n)
+        Strategy: Get the middle element of the LL and reverse the right side and 
+        the left side of the middle element except the head.
+        Start adding nodes to the head first from the right side and then the left           side alternatively and add the middle node at the end."""
         # 1 2 3 4 5
         # 1 3 2 4 5
         # 1 3 4 2 5
@@ -15,19 +19,17 @@ def reorderList(self, head):
         # 1 5 2 4 3
         if head == None:
             return None
-        if head.next == None:
-            return head
-        prev = head
-        curr = head.next
-        while curr != None:
-            temp_prev = prev
-            temp_curr = curr
-            prev.next = curr.next
-            curr.next = temp_prev
-            curr = temp_prev.next
-            prev = temp_curr
-            curr = temp_prev.next
-        # curr = head
-        # while curr != None:
-        #     print curr.val
-        #     curr = curr.next
+        nodes = []
+        curr = head
+        count = 0
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
+            count += 1
+        middle = count // 2
+        
+        for i in range(middle):
+            nodes[i].next = nodes[count - i - 1]
+            nodes[count - i - 1].next = nodes[i + 1]
+        nodes[middle].next = None
+        
