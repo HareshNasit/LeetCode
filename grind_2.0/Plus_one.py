8a6aa3f3448ac96c38a3a4cefb96aa712be56537
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-08-10 23:29:53 -0400

    Started leetcode grind again with Q66 - Easy

---
+++ grind_2.0/Plus_one.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Runtime O(N), Space complexity O(1)
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits)):
+            index = len(digits) - i - 1
+            print(digits[index])
+            if digits[index] + carry == 10:
+                carry = 1
+                digits[index] = 0
+            else:
+                digits[index] = digits[index] + carry
+                carry = 0
+        if carry == 1:
+            digits = [1] + digits
+        return digits
