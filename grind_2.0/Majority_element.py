e06b1aa197bc902386e939084a6706bf75ca740d
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-08-13 23:02:16 -0400

    Completed Q27, Remove Element, Easy, remember for in-place questions use pointers

--- grind_2.0/Majority_element.py
+++ grind_2.0/Majority_element.py
@@ -10,11 +10,3 @@ class Solution:
                 count[num] += 1
             if (count[num] > (nums_length / 2)):
                 return num
-
-        # majority_element = 0
-        # elem_count = 0
-        # for num in count:
-        #     if count[num] > elem_count:
-        #         majority_element = num
-        #         elem_count = count[num]
-        # return majority_element
---
+++ grind_2.0/Remove_element.py
@@ -0,0 +1,25 @@
+class Solution:
+    # https://leetcode.com/problems/remove-element/submissions/
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # Runtime: O(N), Space Complexity: O(1)
+        pointer = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[pointer] = nums[i]
+                pointer += 1
+        return pointer
+
+        # Brute Force O(N^2)
+#         count = 0
+#         for i in range(len(nums)):
+#             if nums[i] == val:
+#                 temp = i + 1
+
+#                 while temp < len(nums) and nums[temp] == val:
+#                     temp += 1
+#                 if temp < len(nums):
+#                     count += 1
+#                     nums[temp], nums[i] = nums[i], nums[temp]
+#             else:
+#                 count += 1
+        # return count
