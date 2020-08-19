36d62687932708c43cba3a171395e4e741a85ed6
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-08-18 22:39:53 -0400

    Alternate solution for Q217

--- grind_2.0/Contains_duplicate.py
+++ grind_2.0/Contains_duplicate.py
@@ -1,6 +1,9 @@
 class Solution:
     # https://leetcode.com/problems/contains-duplicate/
     def containsDuplicate(self, nums: List[int]) -> bool:
+
+        # return len(nums) - len(set(nums)) != 0
+
         # Runtime O(N) and Space Complexity O(N)
         unique = {}
         for elem in nums:
