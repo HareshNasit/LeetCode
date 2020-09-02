c3421b1ce2d52a0a2d68eaa5dc4024e1710c5cc0
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2020-09-01 22:09:37 -0400

    Completed Q34, Find first and last position in sorted array

---
+++ grind_2.0/Find_first_last_position_sorted_array.py
@@ -0,0 +1,42 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # Runtime: O(logN), Space: O(1)
+        # Left index
+        left = 0
+        right = len(nums) - 1
+        positions = []
+        while left < right:
+            mid = (left+right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        if left == len(nums) or nums[left] != target:
+            return [-1,-1]
+
+        positions.append(left)
+        # Right Index
+        left = 0
+        right = len(nums)
+        while left < right:
+            mid = (left+right) // 2
+            if nums[mid] > target:
+                right = mid
+            else:
+                left = mid + 1
+        positions.append(left - 1)
+        return positions
+
+        # Time Complexity: O(N), Naivee Algorithm
+        # starting_index = -1
+        # nums_len = len(nums)
+        # for i in range(nums_len):
+        #     if nums[i] == target:
+        #         starting_index = i
+        #         break
+        # ending_index = -1
+        # for j in range(nums_len - 1, -1, -1):
+        #     if nums[j] == target:
+        #         ending_index = j
+        #         break
+        # return [starting_index, ending_index]
